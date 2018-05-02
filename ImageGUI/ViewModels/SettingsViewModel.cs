94660671e8b9654ccb48a4e8863fae7374e5a7b4
dantepli <dan932@gmail.com>
2018-05-02 15:28:55 +0300

    Remove Button added

--- ImageGUI/ViewModels/SettingsViewModel.cs
+++ ImageGUI/ViewModels/SettingsViewModel.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using ImageServiceGUI.Models;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
 
 namespace ImageServiceGUI.ViewModels
 {
@@ -13,9 +15,18 @@ namespace ImageServiceGUI.ViewModels
     {
         private ISettingsModel model;
         private ObservableCollection<DirectoryPath> m_directoryPaths;
+        private DirectoryPath m_selectedPath;
 
         public ICommand RemoveCommand { get; private set; }
-        public DirectoryPath SelectedPath { get; set; }
+        public DirectoryPath SelectedPath
+        {
+            get { return m_selectedPath; }
+            set
+            {
+                m_selectedPath = value;
+                NotifyPropertyChanged("SelectedPath");
+            }
+        }
 
         public ObservableCollection<DirectoryPath> DirectoryPaths
         {
@@ -31,8 +42,11 @@ namespace ImageServiceGUI.ViewModels
         {
             // TODO assign model
             this.model = null;
-
+            RemoveCommand = new DelegateCommand<object>(OnRemove, CanRemove);
+            this.PropertyChanged += RemoveCommandPropertyChanged;
             m_directoryPaths = new ObservableCollection<DirectoryPath>();
+
+
             m_directoryPaths.Add(new DirectoryPath() { DirPath = "I" });
             m_directoryPaths.Add(new DirectoryPath() { DirPath = "AM" });
             m_directoryPaths.Add(new DirectoryPath() { DirPath = "THE" });
@@ -42,7 +56,17 @@ namespace ImageServiceGUI.ViewModels
             m_directoryPaths.Add(new DirectoryPath() { DirPath = "BOW" });
             m_directoryPaths.Add(new DirectoryPath() { DirPath = "BEFORE" });
             m_directoryPaths.Add(new DirectoryPath() { DirPath = "ME" });
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">sender of the event.</param>
+        /// <param name="e">the event arguments.</param>
+        private void RemoveCommandPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var command = this.RemoveCommand as DelegateCommand<object>;
+            command.RaiseCanExecuteChanged();
         }
 
         public string OutputDir
@@ -65,9 +89,14 @@ namespace ImageServiceGUI.ViewModels
             get { return 120; }
         }
 
+        /// <summary>
+        /// Checks if a directory path was selected.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>true if a directory path was selected.</returns>
         private bool CanRemove(object obj)
         {
-            if(SelectedPath == null)
+            if (SelectedPath == null)
             {
                 return false;
             }
@@ -76,7 +105,9 @@ namespace ImageServiceGUI.ViewModels
 
         private void OnRemove(object obj)
         {
-            // TODO sent path to model
+            // TODO send path to model
+            m_directoryPaths.Remove(SelectedPath);
+            SelectedPath = null;
         }
     }
 }
