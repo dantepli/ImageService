96fc29095c4934260eb8d3445233552dfa69c45f
bar katz <barkatz13897@gmail.com>
2018-05-24 13:03:28 +0300

    added Message type converter to color

---
+++ ImageServiceGUI/Views/TypeToColor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using ImageService.Infrastructure.Enums;
+
+namespace ImageServiceGUI.Views
+{
+    public class TypeToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((MessageTypeEnum) value == MessageTypeEnum.INFO)
+            {
+                return new SolidColorBrush(Colors.Green);
+            } else if((MessageTypeEnum) value == MessageTypeEnum.WARNING)
+            {
+                return new SolidColorBrush(Colors.Yellow);
+            } else
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
